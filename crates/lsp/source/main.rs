e0dd4c5ea01fc7c6602bf0f62ceee24d5ff65144
Aleksei Zarekhta <speicherleck@gmail.com>
2026-08-09 17:32:50 +0500

    feat: add vfs

--- crates/lsp/source/main.rs
+++ crates/lsp/source/main.rs
@@ -1,5 +1,11 @@
+use std::env;
+
 use owo_colors::OwoColorize;
 
 fn main() {
-    println!("{}, {}!", "Hello".red().bold(), "world".red().bold());
+    println!("{} from {}!", "Hello".red().bold(), "lsp".red().bold());
+
+    let arguments: Vec<String> = env::args().collect();
+
+    dbg!(arguments);
 }
---
+++ crates/vfs/source/index.rs
@@ -0,0 +1,48 @@
+use std::vec;
+
+pub struct Index {
+    offsets: Vec<u32>,
+}
+
+impl Index {
+    pub fn new(text: &str) -> Self {
+        u32::try_from(text.len()).expect("too long text");
+
+        let mut offsets: Vec<u32> = vec![0];
+
+        for (i, _) in text.match_indices('\n') {
+            offsets.push((i + 1) as u32);
+        }
+
+        Self { offsets: offsets }
+    }
+
+    pub fn size(&self) -> u32 {
+        self.offsets.len() as u32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn double() {
+        assert_eq!(Index::new("Hello,\r\nworld!").offsets, vec![0, 8]);
+    }
+
+    #[test]
+    fn nothing() {
+        assert_eq!(Index::new("").offsets, vec![0]);
+    }
+
+    #[test]
+    fn single() {
+        assert_eq!(Index::new("Hello, world!").offsets, vec![0]);
+    }
+
+    #[test]
+    fn triple() {
+        assert_eq!(Index::new("Hello\n,\nworld!").offsets, vec![0, 6, 8]);
+    }
+}
